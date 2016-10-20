d5465666b6efc1e7eb61ced05e8d503750ad1963
Kousuke Nagatsuka <koron393@gmail.com>
2016-10-21 08:57:47 +0900

    scorpion: CM'd
    *cm.dependencies file is not provided. Please write to local_manifests.

---
+++ cm-device.mk
@@ -0,0 +1,27 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP621
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP621/SGP621:7.0/23.5.A.0.570/4149872938:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP621-user 7.0 23.5.A.0.570 4149872938 release-keys"
+
+# Permission
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := SGP621,SGP641,scorpion
+
+# Common
+include device/sony/scorpion_windy/cm-device-common.mk
---
+++ cm.mk
@@ -0,0 +1,27 @@
+# Release name
+PRODUCT_RELEASE_NAME := scorpion
+
+# Inherit CM common tablet stuff.
+$(call inherit-product, vendor/cm/configs/common_tablet.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/cm/configs/telephony.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/configs/nfc_enhanced.mk)
+
+# Inherit AOSP scorpion device parts
+$(call inherit-product, device/sony/scorpion/aosp_sgp621.mk)
+
+# Inherit CM scorpion device parts
+$(call inherit-product, device/sony/scorpion/cm-device.mk)
+
+PRODUCT_DEVICE := scorpion
+PRODUCT_NAME := cm_scorpion
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia Z3 Tablet Compact (LTE)
+PRODUCT_MANUFACTURER := Sony
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
