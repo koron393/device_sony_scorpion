80c0e4349f75f8b87fc10f4764f7e2586e361287
Kousuke Nagatsuka <koron393@gmail.com>
2016-10-21 09:02:52 +0900

    scorpion: AICP'd

---
+++ aicp.mk
@@ -0,0 +1,34 @@
+# Release name
+PRODUCT_RELEASE_NAME := scorpion
+
+# Inherit AICP common tablet stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit AOSP scorpion device parts
+$(call inherit-product, device/sony/scorpion/aosp_sgp621.mk)
+
+# Inherit CM scorpion device parts
+$(call inherit-product, device/sony/scorpion/cm-device.mk)
+
+PRODUCT_DEVICE := scorpion
+PRODUCT_NAME := aicp_scorpion
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia Z3 Tablet Compact
+PRODUCT_MANUFACTURER := Sony
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="koron (koron393)"
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
+# Boot animation
+-include vendor/aicp/configs/bootanimation.mk
